6c21e0401340c33d44d275257c3577e8c2867c05
frontendprof <https://github.com/frontendprof>
2021-10-29 23:59:17 +0500

    Routing and links

--- components/banner.js
+++ components/banner.js
@@ -4,13 +4,17 @@ const Banner = (props) => {
   return (
     <div className={s.container}>
       <h1 className={s.title}>
-        <span className={s.title1}>Qahwa</span>
+        <span className={s.title1}>QahwaXona</span>
         <span className={s.title2}>Mushtarikligi</span>
-        <p className={s.subtitle}>Discover your local shops!</p>
+        {/* <span className={s.title1}> قهوه خانه</span>
+        <span className={s.title2}>مشترک </span> */}
+      </h1>
+      <p className={s.subtitle}>Discover your local shops!</p>
+      <div className={s.buttonWrapper}>
         <button className={s.button} onClick={props.handleOnClick}>
           {props.buttonText}
         </button>
-      </h1>
+      </div>
     </div>
   );
 };
